144d196c7a4cffea07bb1e4079ef4322e0fd5fec
hyl2099 <hyl2099@gmail.com>
2020-05-17 15:08:38 +0200

    fix angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,18 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from './app-routing.module';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatFormFieldModule,
-  MatInputModule, MatSelectModule, MatDialogModule
-} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PicturesComponent } from './pictures/pictures.component';
 import {MatCardModule} from '@angular/material/card';
 import {PictureCreateUpdateComponent} from './pictures/picture_creat_update_dialog/picture-create-update.component';
 import {CrudComponent} from './core/crud.component';
-import {MatTableModule} from '@angular/material/typings/esm5/table';
-import {MatIconModule} from '@angular/material/typings/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
 // import { HiComponent } from './hi/hi.component';
 
 @NgModule({
